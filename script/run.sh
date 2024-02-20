f81e86341b365cd183eec6b049d1ac6cd9fed649
Khalob Cognata <khalob@users.noreply.github.com>
2024-02-19 22:07:53 -0500

    Update run.sh

--- script/run.sh
+++ script/run.sh
@@ -2,5 +2,5 @@
 python3 -m wyoming_faster_whisper \
     --model 'distil-small.en' \
     --uri 'tcp://0.0.0.0:10300' \
-    --data-dir './models/faster-distil-whisper-small.en/' \
-    "$@"
+    --data-dir /data \
+    --download-dir /data "$@"
